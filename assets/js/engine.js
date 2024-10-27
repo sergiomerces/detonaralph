1068d8b511e91c5ca8656339cacaf37dcc6205d6
sergiomerces <sergio.merces@gmail.com>
2024-10-27 19:20:28 -0300

    implementando views e values

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,1 +1,38 @@
-alert('Hello world!');
+const state = {
+    view: {
+        squares: document.querySelectorAll('.square'),
+        enemy: document.querySelectorAll('.enemy'),
+        timeLeft: document.querySelectorAll('#time-left'),
+        score: document.querySelectorAll('#score')
+    },
+
+    values: {
+        timerId: null,
+        gameVelocity: 1000
+    }
+};
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove('enemy');
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+
+    randomSquare.classList.add('enemy');
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) =>  {});
+}
+
+function init() {
+    moveEnemy();
+}
+
+init();
